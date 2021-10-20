56aa5fbaedfa6fe670cca93452e20d88e4b0ccac
Iurie Guzun <iurie_guzun@yahoo.com>
2021-10-20 15:20:16 -0400

    Added func getNotes()

--- Notes/Model/Note.swift
+++ Notes/Model/Note.swift
@@ -13,14 +13,14 @@ struct Note {
     static let recordType = "Note"
     let title: String
     
-//    init(title: String) {
-//        self.title = title
-//    }
-//    
-//    init?(record: CKRecord) {
-//        guard let title = record.value(forKey: "title") as? String else { return nil }
-//        self.title = title
-//    }
+    init(title: String) {
+        self.title = title
+    }
+    
+    init?(record: CKRecord) {
+        guard let title = record.value(forKey: "title") as? String else { return nil }
+        self.title = title
+    }
     
     func noteRecord() -> CKRecord {
         let record = CKRecord(recordType: Note.recordType)
